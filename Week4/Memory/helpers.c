faf9d93de9cda945df6b3abeca31360b8bf8ca6e
ankurkhetan2015 <ankurkhetan2615@gmail.com>
2020-04-19 19:33:13 -0500

    Added reflection functionalty and output images

--- Week4/Memory/helpers.c
+++ Week4/Memory/helpers.c
@@ -23,7 +23,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-	int sepiaBlue, sepiaGreen, sepiaRed;
+	int sepiaBlue, sepiaGreen, sepiaRed; // calculate and store new color based on given algorithm
 
     for (int i = 0; i < height; i++)
     {
@@ -33,6 +33,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             sepiaGreen = round(0.168 * image[i][j].rgbtBlue + 0.686 * image[i][j].rgbtGreen + 0.349 * image[i][j].rgbtRed);
             sepiaRed = round(0.189 * image[i][j].rgbtBlue + 0.769 * image[i][j].rgbtGreen + 0.393 * image[i][j].rgbtRed);
 
+            // if resultant color is greater than 255, which is > maximum value for an 8-bit color value cap at 255
             if (sepiaBlue > 255)
             {
                 sepiaBlue = 255;
@@ -48,6 +49,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
                 sepiaRed = 255;
             }
 
+            // sets the original pixel with the new color set of RGB
             image[i][j].rgbtBlue = sepiaBlue;
             image[i][j].rgbtGreen = sepiaGreen;
             image[i][j].rgbtRed = sepiaRed;
@@ -59,6 +61,17 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE swap; // to swap the pixels for a vertical flip (mirror reflection)
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width / 2; j++) // loop till midway of pixels as we can just swap [0][0] and [0][width - 1] and so on
+        {
+            swap = image[i][j];
+            image[i][j] = image[i][width - 1 - j];
+            image[i][width - 1 - j] = swap;
+        }
+    }
     return;
 }
 
