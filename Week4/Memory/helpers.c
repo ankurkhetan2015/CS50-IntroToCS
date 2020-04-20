85bc2ca214bb31f579507882b83cfd49d0b40ccc
ankurkhetan2015 <ankurkhetan2615@gmail.com>
2020-04-20 18:09:21 -0500

    Readded sepia functionality

--- Week4/Memory/helpers.c
+++ Week4/Memory/helpers.c
@@ -30,6 +30,44 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     return;
 }
 
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    int sepiaBlue, sepiaGreen, sepiaRed; // calculate and store new color based on given algorithm
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            sepiaBlue = round(0.131 * image[i][j].rgbtBlue + 0.534 * image[i][j].rgbtGreen + 0.272 * image[i][j].rgbtRed);
+            sepiaGreen = round(0.168 * image[i][j].rgbtBlue + 0.686 * image[i][j].rgbtGreen + 0.349 * image[i][j].rgbtRed);
+            sepiaRed = round(0.189 * image[i][j].rgbtBlue + 0.769 * image[i][j].rgbtGreen + 0.393 * image[i][j].rgbtRed);
+
+            // if resultant color is greater than 255, which is > maximum value for an 8-bit color value cap at 255
+            if (sepiaBlue > 255)
+            {
+                sepiaBlue = 255;
+            }
+
+            if (sepiaGreen > 255)
+            {
+                sepiaGreen = 255;
+            }
+
+            if (sepiaRed > 255)
+            {
+                sepiaRed = 255;
+            }
+
+            // sets the original pixel with the new color set of RGB
+            image[i][j].rgbtBlue = sepiaBlue;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtRed = sepiaRed;
+        }
+    }
+    return;
+}
+
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
